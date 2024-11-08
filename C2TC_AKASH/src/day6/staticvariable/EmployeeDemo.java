a8ad8c38e897403aa32bfffa641116553a8714be
akash <akash@AKASH_SELVA>
2024-11-08 14:44:13 +0530

    static,final

---
+++ C2TC_AKASH/src/day6/staticvariable/EmployeeDemo.java
@@ -0,0 +1,15 @@
+package day6.staticvariable;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e = new Employee("Dharmesh", 29);
+		System.out.println(e);
+		
+		
+		e = new Employee("Dark", 666);
+		System.out.println(e);
+
+	}
+
+}
---
+++ C2TC_AKASH/src/day6/usingfinal/FinalClass.java
@@ -0,0 +1,8 @@
+package day6.usingfinal;
+public final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+
+
